98490d7c8129791b4d5f9eafa6a429607ad671eb
Aleks Svietik <alexeysvietik@gmail.com>
2025-08-26 17:18:23 +0100

    author/painting/user models added

--- a_paintings_project/settings.py
+++ a_paintings_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1a+2ix@fwyf_bdow5cs_9g!i(gr7!m(4-8o3(jzs&fe54xk08='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
